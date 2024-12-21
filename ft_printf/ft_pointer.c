4ac331fb6425142a9b9384a5810f9715fba0cb0d
luli <240750868@qq.com>
2024-12-22 06:15:37 +1030

    2

--- ft_printf/ft_pointer.c
+++ ft_printf/ft_pointer.c
@@ -6,7 +6,7 @@
 /*   By: lli2 <lli2@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 11:17:16 by lli2              #+#    #+#             */
-/*   Updated: 2024/12/22 05:48:32 by lli2             ###   ########.fr       */
+/*   Updated: 2024/12/22 06:15:24 by lli2             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@ int	ft_pointer(void *ptr)
 	int	count;
 
 	count = 0;
-	if (!ptr)
-	{
-		return (ft_putstr("(nil)"));
-	}
 	count += write(1, "0x", 2);
 	count += ft_puthex((unsigned long)ptr, 0);
 	return (count);
