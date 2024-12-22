2e61c3a0a88039a271897bfce251402ec3b23cfd
Lu Li <lli2@F2N001.42adel.org.au>
2024-12-22 14:22:50 +1030

    1

--- ft_printf/ft_pointer.c
+++ ft_printf/ft_pointer.c
@@ -6,7 +6,7 @@
 /*   By: lli2 <lli2@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 11:17:16 by lli2              #+#    #+#             */
-/*   Updated: 2024/12/22 06:15:24 by lli2             ###   ########.fr       */
+/*   Updated: 2024/12/22 14:20:08 by lli2             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ int	ft_pointer(void *ptr)
 {
 	int	count;
 
+	if (ptr == NULL)
+	{
+		return (ft_putstr("(nil)"));
+	}
 	count = 0;
 	count += write(1, "0x", 2);
 	count += ft_puthex((unsigned long)ptr, 0);
